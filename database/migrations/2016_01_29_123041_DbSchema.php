280ceaea7c84095d5934b70f1ac8c03572c98cac
mleboeuf <ml@maximeleboeuf.com>
2016-01-29 07:45:26 -0500

    Db schema

---
+++ database/migrations/2016_01_29_123041_DbSchema.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DbSchema extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('stat');
+            $table->string('source');
+            $table->string('source_url');
+            $table->string('valid_until');
+            $table->int('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('data');
+    }
+}
